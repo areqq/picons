cccaec63aa5b7e1f232493681cb8bbf9ded52b42
areq <areq@amg.net.pl>
2015-12-02 21:11:31 +0100

    simplify

--- channels2ref.py
+++ channels2ref.py
@@ -11,19 +11,20 @@ for l in open('ref2channels.txt'):
         ref, pic = l.split(None, 2)
         R[ref] = pic
 
-
 for p in [ 'picon/100x60/gos_marcin', 'picon/220x132/gos_marcin' ]:
     out = 'out/' + p.replace('/','_')
     if os.path.exists(out):
-	os.rename(out, out + '.old.' + str(time.time())) 
+        os.rename(out, out + '.old.' + str(time.time()))
+    print "Create picons set:", out 
     os.mkdir(out) 
+
     for r in R:
-	spng = p + '/' + R[r] + '.png'
-	dpng = out + '/' + R[r] + '.png'
-	if os.path.exists(spng):
-	    if not os.path.exists(dpng):
-		shutil.copy(spng, dpng)
-	    os.symlink( R[r] + '.png', out + '/' + r + '_0_0_0.png')
-	else:
-	    print "error not found:", spng, "needed by ", r
-	    
+        png = R[r] + '.png'
+        spng = p + '/' + png
+        dpng = out + '/' + png
+        if os.path.exists(spng):
+            if not os.path.exists(dpng):
+                shutil.copy(spng, dpng)
+            os.symlink( png, out + '/' + r + '_0_0_0.png')
+        else:
+            print "error not found:", spng, "needed by ", r
